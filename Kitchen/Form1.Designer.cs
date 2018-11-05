af24631e7ebfe16365811c43df874b31289c0735
N-Nephilim <valerash2@gmail.com>
2018-11-05 12:34:38 +0200

    Initial commit

---
+++ Kitchen/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Kitchen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NAMES = new System.Windows.Forms.TextBox();
+            this.FindRecepts = new System.Windows.Forms.Button();
+            this.BrowseRecepts = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.SuspendLayout();
+            // 
+            // NAMES
+            // 
+            this.NAMES.BackColor = System.Drawing.Color.LightCyan;
+            this.NAMES.Font = new System.Drawing.Font("Calibri", 12F);
+            this.NAMES.Location = new System.Drawing.Point(-8, 0);
+            this.NAMES.Multiline = true;
+            this.NAMES.Name = "NAMES";
+            this.NAMES.Size = new System.Drawing.Size(317, 271);
+            this.NAMES.TabIndex = 0;
+            this.NAMES.TextChanged += new System.EventHandler(this.NAMES_TextChanged);
+            this.NAMES.KeyDown += new System.Windows.Forms.KeyEventHandler(this.NAMES_KeyDown);
+            // 
+            // FindRecepts
+            // 
+            this.FindRecepts.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.FindRecepts.Font = new System.Drawing.Font("Courier New", 12F);
+            this.FindRecepts.Location = new System.Drawing.Point(-8, 247);
+            this.FindRecepts.Name = "FindRecepts";
+            this.FindRecepts.Size = new System.Drawing.Size(317, 73);
+            this.FindRecepts.TabIndex = 1;
+            this.FindRecepts.Text = "Найти рецепты";
+            this.FindRecepts.UseVisualStyleBackColor = false;
+            this.FindRecepts.Click += new System.EventHandler(this.FindRecepts_Click);
+            // 
+            // BrowseRecepts
+            // 
+            this.BrowseRecepts.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BrowseRecepts.Font = new System.Drawing.Font("Courier New", 12F);
+            this.BrowseRecepts.Location = new System.Drawing.Point(-8, 322);
+            this.BrowseRecepts.Name = "BrowseRecepts";
+            this.BrowseRecepts.Size = new System.Drawing.Size(317, 38);
+            this.BrowseRecepts.TabIndex = 2;
+            this.BrowseRecepts.Text = "Просмотреть рецепты";
+            this.BrowseRecepts.UseVisualStyleBackColor = false;
+            this.BrowseRecepts.Click += new System.EventHandler(this.BrowseRecepts_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.LightCyan;
+            this.label1.Font = new System.Drawing.Font("Calibri", 10F);
+            this.label1.Location = new System.Drawing.Point(6, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Впишите ингридиенты...";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(307, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // Form1
+            // 
+            this.AccessibleDescription = "";
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.Indicator;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PowderBlue;
+            this.ClientSize = new System.Drawing.Size(307, 359);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BrowseRecepts);
+            this.Controls.Add(this.FindRecepts);
+            this.Controls.Add(this.NAMES);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximumSize = new System.Drawing.Size(323, 398);
+            this.MinimumSize = new System.Drawing.Size(323, 398);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox NAMES;
+        private System.Windows.Forms.Button FindRecepts;
+        private System.Windows.Forms.Button BrowseRecepts;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+    }
+}
+
---
+++ Kitchen/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kitchen
+{
+    public partial class Form1 : Form
+    {
+        static bool a = false;
+        public Form1()
+        {
+                                                         InitializeComponent();
+                                                        FindRecepts.TabStop = false;
+                                                        FindRecepts.FlatStyle = FlatStyle.Flat;
+                                                        FindRecepts.FlatAppearance.BorderSize = 0;
+                                                        BrowseRecepts.TabStop = false;
+                                                        BrowseRecepts.FlatStyle = FlatStyle.Flat;
+                                                        BrowseRecepts.FlatAppearance.BorderSize = 0;
+            
+        }
+
+        private void FindRecepts_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BrowseRecepts_Click(object sender, EventArgs e)
+        {
+              //  Hide();//Прячет ПЕРВУЮ фрому
+            Form2 f2 = new Form2();
+            f2.StartPosition = FormStartPosition.Manual;
+            f2.Location = this.Location;
+            Hide();
+            f2.ShowDialog();
+            Close();//Закрывает ПЕРВУЮ форму
+        }
+
+        private void NAMES_KeyDown(object sender, KeyEventArgs e)
+        {
+            if ((a == false || NAMES.Text == "") &&(e.KeyCode != Keys.Back && e.KeyCode != Keys.Delete && e.KeyCode != Keys.ShiftKey && e.KeyCode != Keys.Tab && e.KeyCode != Keys.Alt && e.KeyCode != Keys.CapsLock && e.KeyCode != Keys.ControlKey && e.KeyCode != Keys.LWin))
+            {
+                SendKeys.Send("{Enter}");
+                label1.Text = "";
+            }
+            if (e.KeyCode == Keys.Enter)
+            {
+                NAMES.Text = "● " + NAMES.Text;
+                SendKeys.Send("{RIGHT}");
+            }
+            if ((a == false || NAMES.Text == "") && (e.KeyCode != Keys.Back && e.KeyCode != Keys.Delete && e.KeyCode != Keys.ShiftKey && e.KeyCode != Keys.Tab && e.KeyCode != Keys.Alt && e.KeyCode != Keys.CapsLock && e.KeyCode != Keys.ControlKey && e.KeyCode != Keys.LWin))
+            {
+                SendKeys.Send("{RIGHT}"+"{RIGHT}");
+                a = true;
+            }
+        }
+
+        private void NAMES_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
